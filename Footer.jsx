f813a970e5ebbd8f2b7edfd1f870003b21f11f6c
Vidhi Doshi <69749658+Asubtlecoderrr@users.noreply.github.com>
2021-06-07 20:46:25 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  var CurrYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <p>Copyright @ {CurrYear}</p>
+    </footer>
+  );
+}
+
+export default Footer;
